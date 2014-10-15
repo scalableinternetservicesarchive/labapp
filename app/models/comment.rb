9d74123dbf9de808120a39e137973667dbe00255
Bryce Boe <bbzbryce@gmail.com>
2014-10-15 11:51:39 -0700

    [Fix #81220760] Add comment validations.
    Also, clean up comment/submission related views.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :submission
-  belongs_to :parent
+  belongs_to :parent, class_name: 'Comment'
+  validates :submission, presence: true
+  validates :message, length: { minimum: 1 }
+  validates_presence_of :parent, if: 'parent_id.present?'
 end
