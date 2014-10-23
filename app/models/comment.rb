973366e08d1f397e5ce5b81177cd0e8d81c8fe21
Bryce Boe <bbzbryce@gmail.com>
2014-10-23 14:45:50 -0700

    [Fix #81308430] Associated users with comments.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,10 @@
 class Comment < ActiveRecord::Base
   belongs_to :submission
+  belongs_to :user
   belongs_to :parent, class_name: 'Comment'
   has_many :replies, class_name: 'Comment', foreign_key: 'parent_id'
   validates :submission, presence: true
+  validates :user, presence: true
   validates :message, length: { minimum: 1 }
   validates_presence_of :parent, if: 'parent_id.present?'
 end
