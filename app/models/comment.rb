87446fd36860f6907dc3a900dcef4b4f5846dc0d
amutz <andrew.mutz@gmail.com>
2014-11-03 15:22:56 -0800

    adding a randomize option to comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,4 +7,23 @@ class Comment < ActiveRecord::Base
   validates :user, presence: true
   validates :message, length: { minimum: 1 }
   validates_presence_of :parent, if: 'parent_id.present?'
+
+  attr_accessor :randomized
+
+  before_validation do
+    if self.randomized
+
+      self.submission = Submission.order("RANDOM()").first
+      if (rand < 0.8)
+        self.parent = self.submission.comments.to_a.sample
+      else
+        self.parent = nil
+      end
+      self.user = User.order("RANDOM()").first
+
+      self.message = (1+(20*rand).to_i).times.map{ ((7*rand).to_i).times.map { (65+rand(26)).chr }.join}.join(" ")
+    end
+  end
+
+  
 end
