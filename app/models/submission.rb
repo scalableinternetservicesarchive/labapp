c5e417a21db854e27b8547f038a1364e8a2a716a
amutz <andrew.mutz@gmail.com>
2014-11-03 15:07:52 -0800

    adding a randomization option for submissions

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -5,6 +5,16 @@ class Submission < ActiveRecord::Base
   validates :title, length: { minimum: 5 }, uniqueness: true
   validates :url, url: true
 
+  attr_accessor :randomized
+
+  before_validation do
+    if self.randomized
+      self.community = Community.order("RANDOM()").first
+      self.title = 8.times.map { (65+rand(26)).chr }.join
+      self.url = "http://www.cs290.com/#{8.times.map { (65+rand(26)).chr }.join}"
+    end
+  end
+
   def root_comments
     comments.where parent_id: nil
   end
